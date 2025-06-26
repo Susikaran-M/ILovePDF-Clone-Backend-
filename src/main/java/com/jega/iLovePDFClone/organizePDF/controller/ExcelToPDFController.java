414b8f33fe788673106025026df8694e25e374b8
suikaran <Susikaran2005@gmail.com>
2025-06-26 15:05:19 +0530

    added a dependencies

--- src/main/java/com/jega/iLovePDFClone/organizePDF/controller/ExcelToPDFController.java
+++ src/main/java/com/jega/iLovePDFClone/organizePDF/controller/ExcelToPDFController.java
@@ -1,10 +1,8 @@
 package com.jega.iLovePDFClone.organizePDF.controller;
 
-//import org.springframework.stereotype.Controller;
-//
-//@Controller
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ExcelToPDFController {
-	public static void main(String[] args) {
-		System.out.println("Hello");
-	}
+	
 }
---
+++ src/main/java/com/jega/iLovePDFClone/organizePDF/services/ExcelToPDFService.java
@@ -0,0 +1,8 @@
+package com.jega.iLovePDFClone.organizePDF.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExcelToPDFService {
+
+}
