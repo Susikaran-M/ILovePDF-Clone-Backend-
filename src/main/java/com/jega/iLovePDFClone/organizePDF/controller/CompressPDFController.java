3ec8d69c3b730dc5ebdd9ffe2465fb5f4165ceaa
M-vimal <vimalganesh2004@gmail.com>
2025-07-18 12:51:45 +0530

    fix compress

--- src/main/java/com/jega/iLovePDFClone/organizePDF/controller/CompressPDFController.java
+++ src/main/java/com/jega/iLovePDFClone/organizePDF/controller/CompressPDFController.java
@@ -1,31 +1,54 @@
 package com.jega.iLovePDFClone.organizePDF.controller;
 
 
+import java.io.File;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.jega.iLovePDFClone.organizePDF.services.CompressServices;
 import com.jega.iLovePDFClone.organizePDF.services.PDFCompressorService;
+
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/api/pdf")
 public class CompressPDFController {
+	
+	
+	@Autowired
+	CompressServices compressServices;
 
-	 @Autowired
-	    private PDFCompressorService compressorService;
+    @PostMapping("/compress-pdf")
+    public void compressPdf(@RequestParam("file") MultipartFile file,
+                            @RequestParam(value = "level", defaultValue = "screen") String level,
+                            HttpServletResponse response) throws IOException, InterruptedException {
 
-	    @PostMapping("/compress")
-	    public ResponseEntity<byte[]> compressPdf(@RequestParam("file") MultipartFile file) {
-	        try {
-	            byte[] compressedPdf = compressorService.compressPdf(file);
-	            HttpHeaders headers = new HttpHeaders();
-	            headers.setContentType(MediaType.APPLICATION_PDF);
-	            headers.setContentDisposition(ContentDisposition.attachment().filename("compressed.pdf").build());
-	            return new ResponseEntity<>(compressedPdf, headers, HttpStatus.OK);
-	        } catch (Exception e) {
-	            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-	                    .body(("Error compressing PDF: " + e.getMessage()).getBytes());
-	        }
-	    }
+        // Prepare output temp file
+        File compressedFile = File.createTempFile("compressed", ".pdf");
+
+        // Compress using Ghostscript
+        compressServices.compressPdfUsingGhostScript(file, level, compressedFile);
+
+        // Set response headers
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "attachment; filename=compressed.pdf");
+
+        // Stream compressed file to response
+        try (FileInputStream fis = new FileInputStream(compressedFile);
+             ServletOutputStream os = response.getOutputStream()) {
+
+            fis.transferTo(os);
+            os.flush();
+        }
+
+        // Clean up
+        compressedFile.delete();
+    }
 }
